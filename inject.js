ab7b640b217ced09f38a47244011a8538cf550f8
MASh100 <shafi.cse.buet@gmail.com>
2017-02-23 03:39:29 +0600

    Works on webpages except facebook

---
+++ inject.js
@@ -0,0 +1,10 @@
+(function(){
+	var div = document.createElement('div');
+	div.style.position = 'fixed';
+	div.style.top = 0;
+	div.style.right = 0;
+	div.textContent = 'Injected!';
+	document.body.appendChild(div);
+
+	alert('inserted self... giggity');
+})();
